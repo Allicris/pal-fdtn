9cdac23aa88a2915dc87769a7fab5265dcdb9db6
Allison Serrano <allisonserrano171@yahoo.com>
2024-10-01 21:01:31 -0400

    added information and styling to the board page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import "./styles/App.css";
 import Nav from "./components/Nav";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <div style={root}>
     <Nav />
-    <Header />
     <Outlet />
     <Footer />
     </div>
