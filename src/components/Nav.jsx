3dfd1cd5853e8e2c309d6dd00eb848ae2ab5eaf0
Allison Serrano <allisonserrano171@yahoo.com>
2024-10-10 15:00:28 -0400

    added icons to home page and more styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,20 @@
 
 import { Link } from "react-router-dom";
 import "../styles/Nav.css";
+import home from "../../public/icons/home.png";
+import board from "../../public/icons/board.png";
 
 function Nav() {
   return (
-    <ul className="navigation">
-      <Link to="/"><li>Home</li></Link>
-      <Link to="/Board"><li>Board</li></Link>
-    </ul>
+    <div className="navigation">
+      <Link to="/">
+      <img src={home}></img>
+      <p>Home</p>
+      </Link>
+      <Link to="/Board">
+      <img src={board}></img>
+      <p>Board</p></Link>
+    </div>
   );
 }
 
