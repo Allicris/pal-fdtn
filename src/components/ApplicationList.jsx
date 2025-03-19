eb4115aab3d485aab26cf46f4217e9f9bc717fdd
Allison Serrano <allisonserrano171@yahoo.com>
2025-03-19 11:13:54 -0400

    new application upload

--- src/components/ApplicationList.jsx
+++ src/components/ApplicationList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/ApplicationList.css';
 //YOU ONLY NEED TO CHANGE THE NAMES .. NOT THE PATH.
 import spectrum from '../../public/applications/spectrum.pdf';
+import specialized from '../../public/applications/specialized.pdf';
 import ApplicationCard from './ApplicationCard';
 import { handlePrint } from '../utilities/utils';
 
@@ -11,6 +12,11 @@ const ApplicationsList = () => {
       name: "Spectrum for Living",
       website: "https://spectrumforliving.org/",
       fileUrl: spectrum, // this needs to match the import name
+    },
+    {
+      name: "Children's Specialized Hospital",
+      website: "https://childrens-specialized.org/",
+      fileUrl: specialized, // this needs to match the import name
     },
     // { COPY & PASTE THIS SECTION TO ADD ANOTHER APPLICATION, MAKE SURE IT'S INSIDE THE "]" BELOW
     //   name: "Kessler Foundation",
