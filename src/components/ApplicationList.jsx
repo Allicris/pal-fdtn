36088a5a340572dc46a38c5a6ceef29b0321e5c1
Allison Serrano <allisonserrano171@yahoo.com>
2026-02-17 15:43:25 -0500

    update application list

--- src/components/ApplicationList.jsx
+++ src/components/ApplicationList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/ApplicationList.css';
 //YOU ONLY NEED TO CHANGE THE NAMES .. NOT THE PATH.
 import bergen2026 from '../../public/applications/bergen2026.pdf';
-import summit from '../../public/applications/summit.pdf';
+import friedreich2026 from '../../public/applications/friedreich2026.pdf';
 import ApplicationCard from './ApplicationCard';
 import { handlePrint } from '../utilities/utils';
 
@@ -14,9 +14,9 @@ const ApplicationsList = () => {
       fileUrl: bergen2026, // this needs to match the import name
     },
     {
-      name: "Summit Speech School",
-      website: "https://summitspeech.org/",
-      fileUrl: summit,
+      name: "Friedreich's Ataxia Research Alliance",
+      website: "https://curefa.org/",
+      fileUrl: friedreich2026,
 
     },
     // { COPY & PASTE THIS SECTION TO ADD ANOTHER APPLICATION, MAKE SURE IT'S INSIDE THE "]" BELOW
